ed606bdd85f9d7d63a3782fea55658e43e7d914b
Sarthak Thakur <50295928+SarthakThakur0097@users.noreply.github.com>
2020-09-06 11:56:19 -0400

    Cleaned up project

---
+++ BlazorApplication/EmployeeManagement.Models/Department.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace EmployeeManagement.Models
+{
+    public class Department
+    {
+        public int DeperatmentId { get; set; }
+        public string DepartmentName { get; set; }
+    }
+}

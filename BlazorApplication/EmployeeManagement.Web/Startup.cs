374677eca34e70afa4efa63e8be8696c636a101e
Sarthak Thakur <50295928+SarthakThakur0097@users.noreply.github.com>
2020-09-06 17:49:42 -0400

    Implemented Employee REST API Services

--- BlazorApplication/EmployeeManagement.Web/Startup.cs
+++ BlazorApplication/EmployeeManagement.Web/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EmployeeManagement.API.Services;
+using EmployeeManagement.Web.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
@@ -27,6 +29,10 @@ namespace EmployeeManagement.Web
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
+            services.AddHttpClient<IEmployeeService, EmployeeService>(client =>
+            {
+                client.BaseAddress = new Uri("https://localhost:44381/");
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

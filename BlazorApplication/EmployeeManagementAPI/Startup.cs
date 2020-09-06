ddf12e0d7ef6ab694dc4b3eb59c0a9e84de6cfeb
Sarthak Thakur <50295928+SarthakThakur0097@users.noreply.github.com>
2020-09-06 15:20:55 -0400

    Implemented Repository pattern for Employee and Department

--- BlazorApplication/EmployeeManagementAPI/Startup.cs
+++ BlazorApplication/EmployeeManagementAPI/Startup.cs
@@ -2,10 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EmployeeManagement.API.Data;
+using EmployeeManagement.API.Interfaces;
+using EmployeeManagement.API.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,6 +29,11 @@ namespace EmployeeManagementAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IDepartmentRepository, DepartmentRepository>();
+
+            services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+            services.AddDbContext<AppDbContext>(options =>
+                        options.UseSqlServer(Configuration.GetConnectionString("DbConnection")));
             services.AddControllers();
         }
 

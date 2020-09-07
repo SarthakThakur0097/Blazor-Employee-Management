23c71b3b4144553cd49539c62e58cbbf94369f38
Sarthak Thakur <50295928+SarthakThakur0097@users.noreply.github.com>
2020-09-07 11:45:13 -0400

    Implemented EmployeeEditForm Blazor form component

---
+++ BlazorApplication/EmployeeManagement.Web/Pages/EditEmployeeBase.cs
@@ -0,0 +1,20 @@
+﻿using EmployeeManagement.API.Services;
+using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Web.Pages
+{
+    public class EditEmployeeBase : ComponentBase
+    {
+        [Inject]
+        public IEmployeeService EmployeeService { get; set; }
+        public Employee Employee { get; set; } = new Employee();
+        [Parameter]
+        public string Id { get; set; }
+        protected async override Task OnInitializedAsync()
+        {
+            Employee = await EmployeeService.GetEmployee(int.Parse(Id));
+        }
+    }
+}

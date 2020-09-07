88faad84eda35768c4e77ae70029fd33bace3eea
Sarthak Thakur <50295928+SarthakThakur0097@users.noreply.github.com>
2020-09-07 11:54:08 -0400

    Implemented Departments Controller along with Departments Service Classes

--- BlazorApplication/EmployeeManagement.Web/Services/IDepartment.cs
+++ BlazorApplication/EmployeeManagement.Web/Services/IDepartment.cs
@@ -1,12 +1,14 @@
 ﻿using EmployeeManagement.Models;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
-namespace EmployeeManagement.API.Interfaces
+namespace EmployeeManagement.Web.Services
 {
-    public interface IDepartmentRepository
+    public interface IDepartmentService
     {
         Task<IEnumerable<Department>> GetDepartments();
-        Task<Department> GetDepartment(int departmentId);
+        Task<Department> GetDepartment(int id);
     }
 }

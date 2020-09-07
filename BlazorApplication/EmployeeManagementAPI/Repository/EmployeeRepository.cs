2840d0ce53d44f5ffd27d5b8a5f4473cb4a96fe7
Sarthak Thakur <50295928+SarthakThakur0097@users.noreply.github.com>
2020-09-06 21:27:49 -0400

    Included department in Emplyoee Query

--- BlazorApplication/EmployeeManagementAPI/Repository/EmployeeRepository.cs
+++ BlazorApplication/EmployeeManagementAPI/Repository/EmployeeRepository.cs
@@ -42,6 +42,7 @@ namespace EmployeeManagement.API.Repository
         public async Task<Employee> GetEmployee(int employeeId)
         {
             return await appDbContext.Employees
+                .Include(e => e.Department)
                 .FirstOrDefaultAsync(e => e.EmployeeId == employeeId);
         }
         public async Task<Employee> GetEmployeeByEmail(string email)

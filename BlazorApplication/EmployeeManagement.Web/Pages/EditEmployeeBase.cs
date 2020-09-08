a3cd26d50e7176346d106702d27876e872160a46
Sarthak Thakur <50295928+SarthakThakur0097@users.noreply.github.com>
2020-09-08 16:46:12 -0400

    Implemented Update Submission

--- BlazorApplication/EmployeeManagement.Web/Pages/EditEmployeeBase.cs
+++ BlazorApplication/EmployeeManagement.Web/Pages/EditEmployeeBase.cs
@@ -23,6 +23,8 @@ namespace EmployeeManagement.Web.Pages
         public string Id { get; set; }
         [Inject]
         public IMapper Mapper { get; set; }
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
         protected async override Task OnInitializedAsync()
         {
             Employee = await EmployeeService.GetEmployee(int.Parse(Id));
@@ -30,9 +32,16 @@ namespace EmployeeManagement.Web.Pages
 
             Mapper.Map(Employee, EditEmployeeModel);
         }
-        protected void HandleValidSubmit()
+        protected async Task HandleValidSubmit()
         {
+            Mapper.Map(EditEmployeeModel, Employee);
 
+            var result = await EmployeeService.UpdateEmployee(Employee);
+
+            if (result != null)
+            {
+                NavigationManager.NavigateTo("/");
+            }
         }
     }
 }
